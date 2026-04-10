0977c996a81ace2e135fc4e0a11f6482d8afb696
Spriha Anand <21bcs040@nith.ac.in>
2026-04-10 19:26:10 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& t) {
+        int n = t.size();
+        stack<pair<int, int>> st;
+        vector<int> ans(n, 0);
+        //number and ind
+        for(int i = 0; i<n; i++) {
+            if(!st.empty())
+                while(!st.empty() && st.top().first<t[i]){
+                    ans[st.top().second] = i-st.top().second;
+                    st.pop();
+                }
+            st.push(make_pair(t[i], i));
+        }
+        return ans;
+    }
+};
