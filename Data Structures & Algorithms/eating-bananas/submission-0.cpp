2f2c6ed1726385b8bcc2ab071414808f5df4a45c
Spriha Anand <21bcs040@nith.ac.in>
2026-04-12 22:40:57 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1;
+        int r = *max_element(piles.begin(), piles.end());
+        int res = r;
+        while(l<=r){
+            int k = (l+r)/2;
+            int totTime = 0;
+            for(auto p:piles){
+                totTime+=ceil((double)p/k);
+            }
+            if(totTime<=h) {
+                res = k;
+                r = k-1;
+            }
+            else{
+                l = k+1;
+            }
+        }
+        return res;
+    }
+};
