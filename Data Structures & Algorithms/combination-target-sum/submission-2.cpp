b68706be8dae06f769a20ea99f2229ab63bbc84d
Spriha Anand <21bcs040@nith.ac.in>
2026-05-20 19:07:17 +0530

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void help(vector<int>& c, int t, vector<vector<int>>& ans, int ind, vector<int>& temp){
+        if(ind==c.size() || t<0 || c[ind]>t) {
+            if(t==0)
+                ans.push_back(temp);
+            return;
+        }
+        temp.push_back(c[ind]);
+        help(c,t-c[ind], ans, ind, temp);
+        temp.pop_back();
+        help(c,t,ans,ind+1,temp);
+    }
+    vector<vector<int>> combinationSum(vector<int>& c, int t) {
+        sort(c.begin(), c.end());
+        vector<vector<int>> ans;
+        vector<int> temp;
+        help(c, t, ans, 0, temp);
+        return ans;
+    }
+};
