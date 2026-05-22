d08e402b3a5f14b28d06cc5733a565c1c30642da
Spriha Anand <21bcs040@nith.ac.in>
2026-05-22 10:09:14 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void help(vector<string>& ans, int n, int open, int closing, string& temp){
+        if(open+closing == 2*n){
+            ans.push_back(temp);
+            return;
+        }
+        if(open>closing){
+            temp.push_back(')');
+            help(ans, n, open, closing+1, temp);
+            temp.pop_back();
+        }
+        if(open<n){
+            temp.push_back('(');
+            help(ans, n, open+1, closing, temp);
+            temp.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        //a closing bracket can come only if no of open>no of closed
+        //and at the end no of open==no of closed
+        vector<string> ans;
+        string temp = "";
+        help(ans, n, 0, 0, temp);
+        return ans;
+    }
+};
